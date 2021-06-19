aec6b384e983a0185038762042d5bb8253b4601e
atish <bagateatish@gmail.com>
2021-06-19 17:46:28 +0530

    service section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
  import React from 'react'
  //importing pages components 
  import AboutSection from '../components/aboutSection'
+ import ServicesSection from '../components/ServicesSection'
 
  const AboutUs = () => {
     return(
+        <>
         <AboutSection />
+        <ServicesSection />
+        </> 
     )
  }
 
