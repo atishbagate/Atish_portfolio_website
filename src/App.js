f1ac8ba26a5e45946d3e976a52244147b21d44af
atish <bagateatish@gmail.com>
2021-06-19 20:36:08 +0530

    5 About section styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
 //import pages 
 import AboutUs from './pages/AboutUs';
+//golbal style
+import GlobalStyle from './components/GlobalStyle';
 function App() {
   return (
     <div className="App">
+    <GlobalStyle/>
     <AboutUs /> 
     </div>
   );
--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -1,27 +1,59 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                <div className="hide">
+                <Hide>
                     <h2>We work to make</h2>
-                </div>
-                <div className="hide">
+                </Hide>
+                <Hide>
                     <h2>your <span>Drems</span> come</h2>
-                </div>
-                <div className="hide">
+                </Hide>
+                <Hide>
                     <h2>true.</h2>
+                </Hide>
                 </div>
-                </div>
-                <p>Contact us for professional vedio editing for youtube,tiktok,Instagram,or any social media platform.</p>
+                <p>Contact us for professional vedio editing for youtube, tiktok, Instagram <br /> or any social media platform.</p>
                 <button>contact Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="boy with camera." />
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
+
+//styld comonents 
+const About = styled.div`
+min-height: 90vh;
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 5rem 1rem;
+color: white;
+padding-left: 71px;
+`;
+const Description = styled.div`
+flex: 1;
+padding-right: 3rem;
+h2{
+    font-weight:lighter;
+}
+`;
+const Image = styled.div`
+flex:0.7;
+overflow: hidden;
+img{
+    width: 92%;
+    height: 85vh;
+    object-fit: cover;
+}
+`;
+const Hide = styled.div`
+overflow: hidden;
+`;
+
 export default AboutSection;
