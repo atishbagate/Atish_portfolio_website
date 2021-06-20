6f647049f73e2c11ef2a13f86080d796a782a492
atish <bagateatish@gmail.com>
2021-06-20 12:15:14 +0530

    8 nav bar creation and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 //import pages 
 import AboutUs from './pages/AboutUs';
+//import Navbar 
+import Nav from './components/Nav';
 //golbal style
 import GlobalStyle from './components/GlobalStyle';
 function App() {
   return (
     <div className="App">
     <GlobalStyle/>
+    <Nav />
     <AboutUs /> 
     </div>
   );
