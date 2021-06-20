716839dc24fc7ea44c1857f51644bd8f6254e1d3
atish <bagateatish@gmail.com>
2021-06-20 13:14:28 +0530

    adding AboutUs annd Contact and Fixing Router and Switches.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@
 import AboutUs from './pages/AboutUs';
 //import Navbar 
 import Nav from './components/Nav';
+//contact us and our Work import 
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 //golbal style
 import GlobalStyle from './components/GlobalStyle';
+//Router 
+import {Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
     <GlobalStyle/>
     <Nav />
+    <Switch>
+    <Route path="/" exact>
     <AboutUs /> 
+    </Route>
+    <Route path="/work">
+    <OurWork />
+    </Route>
+    <Route path="/contact">
+    <ContactUs />
+    </Route>
+    </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+//import Browser router for DOM
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
+    <BrowserRouter>
         <App />
+    </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
