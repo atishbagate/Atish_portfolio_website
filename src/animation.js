be2e465d43f454ef802dd33dd1340f5bff42753d
atish <bagateatish@gmail.com>
2021-06-22 13:17:22 +0530

    Animation Done on FAQ section. used Toggle (reusable component and added effects on it)

--- src/animation.js
+++ src/animation.js
@@ -1,3 +1,5 @@
+import styled from "styled-components"
+
 export const pageAnimation = {
     hidden:{
         opacity:0,
@@ -7,14 +9,14 @@ export const pageAnimation = {
         opacity:1,
         y:0,
         transition:{
-            duration:1,
+            duration:0.5,
             when:"beforeChildren",
             staggerChildren: 0.25, 
         },
     },
     exit:{
         opacity:0,
-        y:300,
+        // y:300,
         transition:{
             duration:0.5,
         },
@@ -52,6 +54,43 @@ export const photoAnim ={
         transition:{
             ease:"easeOut",
             duration:0.7,
+        }
+    }
+}
+export const lineAnim = {
+    hidden:{
+        width:'0%'
+    },
+    show:{
+        width:'100%',
+        transition:{
+            duration:1,
+        }
+    }
+}
+export const slider = {
+    hidden:{
+        x:'-130%',
+        skew:'45deg',
+    },
+    show:{
+        x:'100%',
+        skew:'0deg',
+        transition:{
+            ease:'easeOut',
+            duration:1
+        }
+    }
+}
+export const sliderContainer = {
+    hidden:{
+        opacity:1
+    },
+    show:{
+        opacity:1,
+        transion:{
+            staggerChildren: 0.15,
+            ease:'easeOut'
         }
     }
 }
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,38 +1,40 @@
 import React from 'react'
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
+import { motion,AnimateSharedLayout } from 'framer-motion'
 const FaqSection = () => {
     return(
         <>
         <Faq>
             <h2>Any Questions <span>Query</span></h2>
+        <AnimateSharedLayout>
+        <Toggle title='How Do I Start vedio editing ?'>
         <div className="quesion">
-            <h4>How Do I Start vedio editing ?</h4>
             <div className="answer">
-                <p>Take a Camera/Mobile start finding best shooting/clicks 
-                views.</p>
+                <p>Take a Camera/Mobile start finding best shooting/clicks views.</p>
                 <p>use youtube tutorials for basic clearance.</p>
             </div>
-            <div className="faq-line"></div>
         </div>
+        </Toggle>
+        <Toggle title='How see our projects ?'> 
         <div className="quesion">
-            <h4>How see our projects ?</h4>
             <div className="answer">
                 <p>follow us on social media handle.</p>
                 <p>we make a daily post on our recently work/project.</p>
             </div>
-            <div className="faq-line"></div>
         </div>
+        </Toggle>
+        <Toggle title='How to Make payment ?'>
         <div className="quesion">
-            <h4>How to Make payment ?</h4>
             <div className="answer">
                 <p>We accept all type of payments.</p>
                 <p>Cash cards, UPI, RTGS, NEFT, internet banking, etc</p>
             </div>
-            <div className="faq-line"></div>
         </div>
+        </Toggle>
+        </AnimateSharedLayout>
         </Faq>
-        
         </>
     )
 }
